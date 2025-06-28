749ae3c61f8b17d8642dedaf28fa82c04e19960e
Tharindu Nuwan <tharindunm94@gmail.com>
2025-06-28 11:11:01 +0530

    initial commit

---
+++ Layered_Architecture_Project/Models/StudentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layered_Architecture_Project.Models
+{
+    class StudentModel
+    {
+    }
+}
